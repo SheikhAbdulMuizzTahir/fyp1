ef315e3a2771c937d98b993aba56de5a90febf27
cinderallaman1 <ahmaddddddddddkhan@gmail.com>
2023-06-10 19:56:32 +0500

    bla

--- routes/Model.js
+++ routes/Model.js
@@ -1,6 +1,7 @@
 import Express from "express";
 import { Model } from "../models/Models.js";
-import { updateGallery } from "../controllers/modelsController.js";
+import { getAllModels, updateGallery } from "../controllers/modelsController.js";
 export const router = Express.Router();
 
 router.post("/updateGallery/:id", updateGallery);
+router.get("/", getAllModels);
